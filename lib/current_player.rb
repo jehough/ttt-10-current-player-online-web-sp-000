888f91ab52374ebedf77d5b6a5216a99ce5dfe11
Jim Hough <teachlife@outlook.com>
2019-04-13 00:49:44 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -5,6 +5,10 @@ def turn_count (board)
     counter += 1
 end
 
-def current_player
-  
+def current_player (board)
+  if turn_count (board) % 2 == 0
+    player = "X"
+  else
+    player = "O"
+  end
 end
